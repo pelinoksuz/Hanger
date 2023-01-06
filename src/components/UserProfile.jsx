1564b2b4bfec86cad40734fe8b3beb8b5d08e5df
Pelin Oksuz <Pelin.Oksuz@avl.com>
2023-01-06 20:44:28 +0300

    chat icon navigation added to the profile page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,23 +1,23 @@
-import React, { useContext, useState,useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../Firebase";
 import { AuthContext } from "../context/AuthContext";
-import Eventform from "./Eventform"
-import { doc, onSnapshot } from "firebase/firestore"; 
+import Eventform from "./Eventform";
+import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "../Firebase";
-
+import chat from "../img/chat.png";
 const UserProfile = () => {
-  const [myevent,setmyevents] = useState("");
+  const [myevent, setmyevents] = useState("");
   const { currentUser } = useContext(AuthContext);
 
-  const [popup, setPopup] = useState('close');
+  const [popup, setPopup] = useState("close");
 
   const [name, setname] = useState("Tennis");
   const [location, setlocation] = useState("Tennis Club Atasehir");
-  
+
   const [desc, setdesc] = useState("");
-  const open = () =>{
-    switch(popup){
+  const open = () => {
+    switch (popup) {
       case "close":
         setPopup("open");
         return;
@@ -27,7 +27,7 @@ const UserProfile = () => {
       default:
         setPopup("close");
         return;
-    };
+    }
   };
 
   useEffect(() => {
@@ -49,13 +49,17 @@ const UserProfile = () => {
       <div className="navbar_p">
         <div className="user_p">
           <img src={currentUser.photoURL} alt="" />
-          
         </div>
+        <nav className="chat">
+          <a href="/">
+            <img src={chat} alt="" />
+          </a>
+        </nav>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
       <div className="navbar_p">
         <div className="user_p">
-        <h1 className="logo_p">{currentUser.displayName}</h1>
+          <h1 className="logo_p">{currentUser.displayName}</h1>
         </div>
       </div>
       <div className="navbar_p">
@@ -64,23 +68,29 @@ const UserProfile = () => {
         </div>
       </div>
 
-
       <button onClick={(e) => open()}>Create new event</button>
-      <Eventform somepop ={popup}/>
+      <Eventform somepop={popup} />
 
       <div className="events">
-        <h3 className="logo_p">Your Event List:</h3>
-        {myevent.events&&myevent.events.map((item,key)=><span key={key}>
-          
-          <span className="event">Event Name : {myevent.events[key].name}</span>
-          <span className="event">Location : {myevent.events[key].location}</span>
-          <span className="event">Desc : {myevent.events[key].description}</span>
-          <span className="event">Date : {myevent.events[key].date}</span>
-        </span>
-        )}
-      
-      
-    </div>
+        {popup === "close" ? <h3 className="logo_p">Your Event List:</h3> : ""}
+
+        {myevent.events &&
+          popup === "close" &&
+          myevent.events.map((item, key) => (
+            <span key={key}>
+              <span className="event">
+                Event Name : {myevent.events[key].name}
+              </span>
+              <span className="event">
+                Location : {myevent.events[key].location}
+              </span>
+              <span className="event">
+                Desc : {myevent.events[key].description}
+              </span>
+              <span className="event">Date : {myevent.events[key].date}</span>
+            </span>
+          ))}
+      </div>
     </div>
   );
 };
